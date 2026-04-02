9f532358cf0fad3b9e211e53f7eb5646973be8cf
Josiah-Rigg <josiah.rigg@stu.ocsb.ca>
2026-04-02 13:27:05 -0400

    Changed comments

--- main.ts
+++ main.ts
@@ -14,30 +14,30 @@ basic.showIcon(IconNames.Happy)
 myNeopixelStrip.clear()
 myNeopixelStrip.show()
 
-// code for lighting up neopixels
-input.onButtonPressed(Button.A, function () {
-    lightLevels = input.lightLevel()
+  // code for lighting up neopixels
+  input.onButtonPressed(Button.A, function () {
+  lightLevels = input.lightLevel()
 
-    // shows the current light level
-    basic.showNumber(lightLevels)
-    myNeopixelStrip.clear()
+  // shows the current light level
+  basic.showNumber(lightLevels)
+  myNeopixelStrip.clear()
 
-// if lightlevel is greater then 52 the color turns red 
+  // if lightlevel is greater then 52 the color turns red 
   if (lightLevels > 52) {
     myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
   }
 
-// if lightlevel is greater then 104 the color turns red 
+  // if lightlevel is greater then 104 the color turns red 
   if (lightLevels > 104) {
     myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
   }
 
-// if lightlevel is greater then 156 the color turns red 
+  // if lightlevel is greater then 156 the color turns red 
   if (lightLevels > 156) {
     myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
   }
     
-// if lightlevel is greater then 208 the color turns red 
+  // if lightlevel is greater then 208 the color turns red 
   if (lightLevels > 208) {
     myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
   }
